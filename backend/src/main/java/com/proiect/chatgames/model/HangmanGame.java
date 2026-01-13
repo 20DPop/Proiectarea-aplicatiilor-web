8a8f2f5ecf693024e8278fa605d7c91d41a108b9
Danut <deny40167@gmail.com>
2026-01-13 11:39:36 +0200

    documentatie

--- backend/src/main/java/com/proiect/chatgames/model/HangmanGame.java
+++ backend/src/main/java/com/proiect/chatgames/model/HangmanGame.java
@@ -12,7 +12,6 @@ public class HangmanGame {
     private String hostUsername;
     private String guesserUsername;
 
-    // Date secrete (nu le trimitem la frontend decat la final)
     private String secretWord;
 
     // Starea curenta
@@ -20,7 +19,6 @@ public class HangmanGame {
     private int mistakes = 0;
     private final int maxGuesses = 6;
 
-    // Statusuri posibile: "waiting_for_guesser", "waiting_for_word", "in_progress", "won", "lost"
     private String status = "waiting_for_guesser";
 
     // Aceasta e proprietatea calculata pe care o vede frontend-ul (ex: "_ A _ _")
